7fc3a6f106f6e72e45531cfa5304303039511c65
Christopher Vagnetoft <noccylabs@gmail.com>
2010-08-04 14:57:41 +0000

    Graphics stubs

---
+++ sys/lepton/graphics/canvas.php
@@ -0,0 +1,8 @@
+<?php
+
+	// Canvas Implementation
+
+
+
+
+?>
---
+++ sys/lepton/graphics/colorspace/rgb.php
@@ -0,0 +1,13 @@
+<?php
+
+ModuleManager::load('lepton.graphics.colorspaces');
+
+class RgbColorSpace extends ColorSpace {
+
+	private $r;
+	private $g;
+	private $b;
+
+}
+
+?>
