7ba02c745287683de7ffe31555c758a54a4d4a69
Christopher Vagnetoft <noccylabs@gmail.com>
2010-10-30 17:16:15 +0000

    Committing initial bbcode markup parser and markup parser base class. Needs a bit more work

---
+++ sys/lepton/web/markup.php
@@ -0,0 +1,132 @@
+<?php __fileinfo("Markup Parser Core",
+    array(
+        'version' => '1.0'
+    )
+);
+
+/**
+ * @brief Interface for markup parsers.
+ * Normally inherited via the abstract base class MarkupParser.
+ *
+ * @package lepton.web.markup
+ * @see MarkupParser
+ * @author Christopher Vagnetoft <noccy@chillat.net>
+ */
+interface IMarkupParser {
+    /**
+     * @brief Parse the data and return the output
+     * @param string $data The data to parse
+     */
+    function parse($data);
+}
+
+/**
+ * @brief Abstract base class for markup parsers
+ *
+ * @package lepton.web.markup
+ * @see IMarkupParser
+ */
+abstract class MarkupParser implements IMarkupParser {
+    protected $data = null;
+    protected $options = null;
+    protected $parsed = null;
+
+    /**
+     * @brief Markup parser constructor
+     *
+     * @see MarkupParser::setData
+     * @see MarkupParser::getOptions
+     * @see MarkupParser::getOption
+     * @param string $string The markup text to process
+     * @param array $options The options to apply
+     */
+    function __construct($string=null,array $options=null) {
+        $this->data = $string;
+        $this->options = (($options==null)?array():(array)$options);
+        $this->parsed = $this->parse($this->data);
+    }
+
+    /**
+     * @brief String casting method.
+     *
+     * @see MarkupParser::getParsed
+     * @return string The parsed markup
+     */
+    function __toString() {
+        return $this->getParsed();
+    }
+
+    /**
+     * @brief Assign the data and options to the parser.
+     *
+     * @param string $string The string to parse
+     * @param array $options The options to assign to the parser.
+     */
+    function setData($string,array $options=null) {
+        $this->data = $string;
+        if ($options) {
+            $this->options = (($options==null)?array():(array)$options);
+        }
+        $this->parsed = $this->parse($this->data);
+    }
+
+    /**
+     * @brief Return the data to be parsed.
+     *
+     * @return string The data assigned.
+     */
+    function getData() {
+        return $this->data;
+    }
+
+    /**
+     * @brief Returns the applied options.
+     *
+     * @return array The options
+     */
+    function getOptions() {
+        return $this->options;
+    }
+
+    /**
+     * @brief Return the parsed data
+     *
+     * @return string The parsed data
+     */
+    function getParsed() {
+        return $this->parsed;
+    }
+
+    /**
+     * @brief Return an option set in the constructor or using setData
+     *
+     * @see MarkupParser::setData
+     * @param string $option The option to query
+     * @param mixed $default Default value to return if not present
+     * @return mixed The option value or the default value
+     */
+    function getOption($option,$default=null) {
+        if (isset($this->options[$option])) {
+            return $this->options[$option];
+        } else {
+            return $default;
+        }
+    }
+
+    /**
+     * @brief Factory method to create a parser and feed it relevant data.
+     *
+     * @param string $parser The parser to create, ex. "bbcode"
+     * @param string $string The string to parse
+     * @param array $options The options to assign to the parser
+     * @return MarkupParser The parser instance
+     */
+    static function factory($parser,$string,$options=null) {
+        $parserclass = $parser.'MarkupParser';
+        if (class_exists($parserclass)) {
+            return new $parserclass($string,$options);
+        }
+        throw new ClassNotFoundException("Could not find markup parser");
+    }
+}
+
---
+++ sys/lepton/web/markup/bbcode.php
@@ -0,0 +1,52 @@
+<?php __fileinfo("Markup Parser: bbCode", array(
+    'depends' => array(
+        'lepton.web.markup'
+    )
+));
+
+/**
+ * @brief Handles bbCode markup
+ *
+ * @package lepton.web.markup
+ * @author Christopher Vagnetoft <noccy@chillat.net>
+ */
+class BBCodeMarkupParser extends MarkupParser {
+    /// If set to true, links including protocols will have the rel="nofollow" attribute set
+    const OPT_EXTERNAL_NOFOLLOW = 'nofollow';
+    /// An array containing the protocols that are allowed
+    const OPT_ALLOWED_PROTOCOLS = 'protocols';
+    /// If true, the output will be formatted as xhtml
+    const OPT_OUTPUT_XHTML = 'xhtml';
+    static $defaults = array(
+        self::OPT_EXTERNAL_NOFOLLOW => true,
+        self::OPT_ALLOWED_PROTOCOLS => array(
+            'http',
+            'https',
+            'ftp'
+        ),
+        self::OPT_OUTPUT_XHTML => false
+    );
+
+    /**
+     * @brief Parse the bbcoded data and return html
+     *
+     * @param string $data The data to parse
+     * @return string The parsed data
+     */
+    function parse($data) {
+        $res = $data;
+        $res = preg_replace('/\[b\](.*)\[\/b\]/i', '<font style="font-weight:bold;">$1</font>', $res);
+        $res = preg_replace('/\[u\](.*)\[\/u\]/i', '<font style="text-decoration:underline;">$1</font>', $res);
+        $res = preg_replace('/\[i\](.*)\[\/i\]/i', '<font style="font-style:italic;">$1</font>', $res);
+
+        // Split on double newlines
+        $res = explode("\n\n", $res);
+        $out = array();
+        // Replace the remaining newlines
+        foreach($res as $resrow) {
+            $out[] = str_replace("\n","", $resrow);
+        }
+        $res = '<p>'.join('</p><p>',$out).'</p>';
+        return $res;
+    }
+}
