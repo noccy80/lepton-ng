b1bfc277e7c2eb66037627a2490e0f03f7a2a494
Christopher Vagnetoft <noccylabs@gmail.com>
2010-11-19 16:45:22 +0000

    Adding schema for wikipages

---
+++ dist/sql/wikipages.sql
@@ -0,0 +1,13 @@
+CREATE TABLE wikipages (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pagens VARCHAR(64) NOT NULL,
+    pagename VARCHAR(255) NOT NULL,
+    pagetitle VARCHAR(255) NOT NULL,
+    revision INT NOT NULL DEFAULT 1,
+    content TEXT,
+    markuptype VARCHAR(32) NOT NULL DEFAULT 'wiki',
+    created DATETIME NOT NULL,
+    locked TINYINT NOT NULL DEFAULT 0,
+    author INT NOT NULL,
+    UNIQUE KEY pageid(pagens,pagename,revision)
+);
