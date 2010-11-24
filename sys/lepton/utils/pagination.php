0141c516a3ae41ab95da4d273499bb6790dc9267
Christopher Vagnetoft <noccylabs@gmail.com>
2010-11-24 21:47:41 +0000

    Initial pagination classes in place

---
+++ sys/lepton/utils/pagination.php
@@ -0,0 +1,13 @@
+<?php __fileinfo("Pagination classes");
+
+class Paginator {
+	private $itemsperpage;
+	function __construct($itemsperpage) {
+		$this->itemsperpage = $itemsperpage;
+		$this->numitems = $numitems;
+	}
+	function getNumPages($numitems) {
+		return ceil(($numitems - 1) / $this->itemsperpage)) + 1;
+	}
+}
+
