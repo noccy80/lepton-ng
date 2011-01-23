42592974c98843e5eca1bfbb9909c02a6848f8e7
Christopher Vagnetoft <noccylabs@gmail.com>
2011-01-23 17:02:04 +0000

    Updated actions class

--- sys/lepton/cli/action.php
+++ sys/lepton/cli/action.php
@@ -22,21 +22,26 @@ abstract class Actions {
 				console::writeLn("Valid commands for %s:", $command);
 				foreach(self::$_actions[$command]['commands'] as $cmd=>$data) {
 					$argstr = $data['arguments'];
-					console::writeLn("    %s %s -- %s", $cmd, __astr($argstr),$data['info']);
+					console::writeLn("    %s %s  - %s", __astr('\b{'.$cmd.'}'), __astr($argstr),$data['info']);
 				}
 				return true;
 			} else {
-				return call_user_func_array(
-					array(self::$_actions[$command]['handler'], $arguments[0]),
-					array_slice($arguments,1)
-				);
+				if (method_exists(self::$_actions[$command]['handler'], $arguments[0])) {
+					call_user_func_array(
+						array(self::$_actions[$command]['handler'], $arguments[0]),
+						array_slice($arguments,1)
+					);
+					return true;
+				} else {
+					return false;
+				}
 			}
 		}
 	}
 	public static function listActions() {
 		console::writeLn("Valid actions:");
 		foreach(self::$_actions as $action=>$data) {
-			console::writeLn("    %s - %s", $action, $data['info']);
+			console::writeLn("    %s: %s", __astr('\b{'.$action.'}'), $data['info']);
 		}
 	}
 }
