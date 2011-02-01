11ca1192cd796d5249da6b97a3d5c66ea29a4f50
Christopher Vagnetoft <noccylabs@gmail.com>
2011-02-01 14:48:17 +0000

    Fixed missing semicolon in statictext create statement

--- dist/sql/statictext.sql
+++ dist/sql/statictext.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS statictext;
 CREATE TABLE statictext (
-	id int(11) NOT NULL AUTO_INCREMENT,
+	id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	slug varchar(64) NOT NULL,
 	datemodified datetime NOT NULL,
 	content text,
-	PRIMARY KEY (id),
 	UNIQUE KEY slug (slug)
-)
+);
