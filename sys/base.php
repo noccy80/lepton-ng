2d2f50425f8d61b6dd7a778e71d3a2955b55c518
Christopher Vagnetoft <noccylabs@gmail.com>
2011-02-15 12:34:49 +0000

    Added debug::inspectTable()

--- sys/base.php
+++ sys/base.php
@@ -1425,6 +1425,16 @@ class Debug {
         die();
     }
 
+    static function inspectTable(array $table) {
+        echo '<style type="text/css">';
+        echo 'table { font:12px sans-serif; border-collapse:collapse; border:solid 1px #BBB; width:100%; }';
+        echo 'th { text-align:left; padding:3px; border:solid 1px #BBB; background-color:#EEE; width:10%; }';
+        echo 'td { padding:3px; border:solid 1px #BBB}';
+        echo '</style>';
+        echo debug::inspectTableArray($table);
+        die();
+    }
+
     static function inspectArray($data) {
         $ret = '<table>';
         foreach ($data as $key => $value) {
@@ -1435,6 +1445,36 @@ class Debug {
                 $ret.= htmlentities($value);
             }
             $ret.='</td></tr>';
+        }
+        $ret.= '</table>';
+        return $ret;
+    }
+
+    static function inspectTableArray($data) {
+        $skipnum = true;
+        $ret = '<table>';
+        $head = $data[0];
+        $ret.= '<tr>';
+        $ret.= '<th>&nbsp;</th>';
+        $keys = array_keys($head);
+        for($idx = 0; $idx < count($keys); $idx+=2) {
+            $col = $keys[$idx];
+            $ret.='<th>'.$col.'</th>';
+        }
+        $ret.= '</tr>';
+        $idx = 0;
+        for($rowidx = 0; $rowidx < count($data); $rowidx++) {
+            $ret.='<tr><th>' . $rowidx . '</th>';
+            $row = $data[$rowidx]; $rowdata = array_values($row);
+            for ($col = 0; $col < (count($row) / 2); $col++) {
+                $value = $row[$col];
+                if (is_array($value)) {
+                    $ret.= '<td>'.debug::inspectTableArray($value).'</td>';
+                } else {
+                    $ret.= '<td>'.htmlentities($value).'</td>';
+                }
+            }
+            $ret.='</tr>';
         }
         $ret.= '</table>';
         return $ret;
