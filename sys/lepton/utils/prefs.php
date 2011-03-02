8b6a8445804cc477000736983add238d328146f1
Christopher Vagnetoft <noccylabs@gmail.com>
2011-03-02 01:24:54 +0000

    Added database backend to prefs class (using DbPrefs as opposed to FsPrefs)

--- sys/lepton/utils/prefs.php
+++ sys/lepton/utils/prefs.php
@@ -59,3 +59,35 @@ class FsPrefs extends Prefs {
 	}
 
 }
+
+class DbPrefs extends Prefs {
+
+	private $table;
+	private $db;
+
+	public function __construct($table) {
+		$this->db = new DatabaseConnection();
+		$this->table = $table;
+		try {
+			$tcheck = $this->db->getSingleRow("SHOW CREATE TABLE ".$this->table);
+		} catch(Exception $e) {
+			$tcheck = null;
+		}
+		$this->data = array();
+		if (!$tcheck) {
+			$this->db->exec("CREATE TABLE ".$this->table." (prefskey VARCHAR(64) NOT NULL PRIMARY KEY, data BLOB)");
+		} else {
+			$keys = $this->db->getRows("SELECT * FROM ".$this->table);
+			foreach((array)$keys as $row) {
+				$this->data[$row['prefskey']] = unserialize($row['data']);
+			}
+		}
+	}
+
+	public function flush() {
+		foreach($this->data as $key=>$value) {
+			$this->db->updateRow("REPLACE INTO ".$this->table." (prefskey,data) VALUES (%s,%s)", $key, serialize($value));
+		}
+	}
+	
+}
