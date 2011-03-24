61181adf18703cfa12a6776650d673bfe21eca60
Christopher Vagnetoft <noccylabs@gmail.com>
2011-03-24 01:18:57 +0000

    Initial checkin of disqus test code and element dom wrapper classes

---
+++ sys/lepton/web/element.php
@@ -0,0 +1,59 @@
+<?php
+
+class Element {
+
+    private $_elem = null;
+    private $_args = array();
+    private $_content = null;
+    // TODO: Extend
+    const contentelem = array('p','div','a','em','i','u','script');
+
+    function __callstatic($elem,array $args=null,$content=null) {
+        $el = new Element($elem,$args);
+        $el->setContent($content);
+        return $el;
+    }
+    
+    function __construct($elem,array $args=null) {
+        $this->_elem = $elem;
+        $this->_args = (array)$args;
+    }
+    
+    function appendChild(Element $elem) {
+        if (!is_array($this->_content)) {
+            $this->_content = array();
+            // TODO: Should really warn about the content being replaced as
+            // long as it was not null.
+        }
+        $this->_content[] = $elem;
+        
+    }
+    
+    function setContent($content) {
+        $this->_content = $content;
+    }
+    
+    function __toString() {
+        $attrl = array();
+        foreach($attr as $key=>$val) {
+            $attrl[] = sprintf('%s="%s"', $key, htmlentities($val));
+        }
+        if (is_array($this->_content)) {
+            $contl = array();
+            foreach($this->_content as $node) {
+                $contl[] = (string)$node;
+            }
+            $conts = join('',$contl);
+        } elseif ($this->_content != null) {
+            $conts = $this->_content;
+        } else {
+            $conts = '';
+        }
+        if (arr::hasValue(self::contentelem, $this->_elem)) {
+            $node = sprintf('<%s %s>%s</%s>', $this->_elem, $attrl, $conts);
+        } else {
+            $node = sprintf('<%s %s>', $this->_elem, $attrl);
+        }
+    }
+
+}
---
+++ sys/lepton/web/widgets/disqus.php
@@ -0,0 +1,28 @@
+<?php __fileinfo("Disqus Widget");
+
+using('lepton.web.element');
+
+class DiscusWidget extends HtmlWidget {
+    function __construct(array $params) {
+        $this->_shortname = $params['shortname'];
+        $this->_identifier = $params['identifier'];
+        $this->_permalink = $params['permalink'];
+    }
+    function render() {
+        echo '<div id="disqus_thread"></div>';
+        echo '<script type="text/javascript">';
+        echo '    var disqus_shortname = \''.$this->_shortname.'\';';
+        echo '    var disqus_identifier = \''.$this->_identifier.'\';';
+        echo '    var disqus_url = \''.$this->_permalink.'\';';
+        echo '(function() { ';
+        echo "var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;";
+        echo "dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';";
+        echo "(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);";
+        echo '})();';
+        echo '</script>';
+        echo '<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>';
+        echo '<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>';
+        
+    }
+}
+
