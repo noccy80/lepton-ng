b5534fc18dfdfd86d707efe62537dd7356046887
Christopher Vagnetoft <noccylabs@gmail.com>
2011-05-17 16:24:46 +0000

    Fixed: Rasterizer now functional with callback for transformations

--- sys/lepton/graphics/rasterizer.php
+++ sys/lepton/graphics/rasterizer.php
@@ -1,28 +1,68 @@
 <?php
 
+using('lepton.graphics.canvas');
+
+/**
+ * @class SvgRasterizer
+ * @brief Rasterizes SVG images into a usable canvas.
+ * 
+ * This function will allow for loading and transforming an SVG image before
+ * rasterizing it into a canvas. Modifications can be done to the canvas using
+ * the usual tools (filters, painters, fonts etc) but any modification of the
+ * SVG source using the transformSvg method will reset the canvas to that of
+ * the rasterized state.
+ * 
+ * @author Christopher Vagnetoft
+ * @todo Implement a fallback on the imagick binaries if extension not found
+ */
 class SvgRasterizer extends Canvas {
 	
-	function __construct($svgfile,$width,$height) {
-		$im = new Imagick();
-		$svgin = file_get_contents($svgfile);
+	private $svg = null;
+	private $imh = null;
+	
+	function __construct($svgfile) {
+		
+		$this->imh = new Imagick();
+		$this->svg = file_get_contents($svgfile);
 
-		/*loop to color each state as needed, something like 
-		1)explode $svgin
-		2)foreach($array as $state){preg_replace blank color->state color }
-		3)implode to $svgout*/
-		$svgout = $svgin;
+		$this->svgToImage();
+		// $im->writeImage('/path/to/colored/us-map.png');/*(or .jpg)*/
 
-		$im->readImageBlob($svgout);
+	}
+	
+	/**
+	 * @brief Transform the XML of the SVG image using a function.
+	 * 
+	 * The function will be invoked with one parameter being the XML of the
+	 * SVG image, and is expected to return the transformed XML. Not returning
+	 * anything will leave the canvas intact and return false.
+	 * 
+	 * @param Function $transformfunction A function to do the transformation
+	 * @return Bool True if the operation was successful
+	 */
+	public function transformSvg($transformfunction) {
+		$xml = $this->svg;
+		$xml = call_user_func_array($transformfunction,array($xml));
+		if (!$xml) return false;
+		$this->svg = $xml;
+		// Update the image
+		$this->svgToImage();
+		return true;
+	}
+	
+	private function svgToImage() {
 
-		/*png settings*/
-		$im->setImageFormat("png24");
-		$im->resizeImage($width, $height, imagick::FILTER_LANCZOS, 1);  /*Optional, if you need to resize*/
+		$this->imh->readImageBlob($this->svg);
 
-		/*jpeg*/
-		// $im->setImageFormat("jpeg");
-		// $im->adaptiveResizeImage(720, 445); /*Optional, if you need to resize*/
+		$this->imh->setImageFormat("png24");
+		/*
+		if ($height && $width) {
+			$this->imh->resizeImage($width, $height, imagick::FILTER_LANCZOS, 1);
+			$im->adaptiveResizeImage($width, $height);
+		}
+		*/
 
-		$img = @imagecreatefromstring((string)$im);
+		$img = @imagecreatefromstring((string)$this->imh);
 		if ($img) {
 			$this->setImage($img);
 			$this->gotimage = true;
@@ -30,10 +70,14 @@ class SvgRasterizer extends Canvas {
 			throw new GraphicsException("Failed to load the image.", GraphicsException::ERR_LOAD_FAILURE);
 		}
 		
-		// $im->writeImage('/path/to/colored/us-map.png');/*(or .jpg)*/
-		$im->clear();
-		$im->destroy();		
 	}
 	
+	function __destruct() {
+		$this->imh->clear();
+		$this->imh->destroy();		
+		unset($this->imh);
+		
+		parent::__destruct();
+	}
 	
 }
