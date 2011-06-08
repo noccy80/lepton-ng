77f22542e622fcc4eca80e45bac68f9abeca1a38
Christopher Vagnetoft <noccylabs@gmail.com>
2011-06-08 21:02:28 +0000

    Added: Prime number generator and math class (lepton.math)

---
+++ sys/lepton/math.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class Math {
+
+	public static function generatePrimes($n) {
+		if ($n == 2) return array(2);
+		if ($n < 2) return array();
+		$s = range(3, $n + 1, 2);
+		$mroot = $n ^ 0.5;
+		$half = ($n + 1) / 2 - 1;
+		$i = 0;
+		$m = 3;
+		while ($m <= $mroot) {
+			if ($s[$i]) {
+				$j = ($m * $m - 3) / 2;
+				$s[$j] = 0;
+				while ($j < $half) {
+					$s[$j] = 0;
+					$j += $m;
+				}
+			}
+			$i++;
+			$m = 2 * $i + 3;
+		}
+		$ret = array();
+		foreach($s as $v) if ($v) $ret[] = $v;
+		return $ret;
+	}
+
+}
